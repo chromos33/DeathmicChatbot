e3a84771ee22c264c469e2888108aa6f3a42b21d
Deathmic <deathmic@gmail.com>
2013-08-15 17:29:26 +0200

    Added saving list of streams to local file.

--- DeathmicChatbot/TwitchManager.cs
+++ DeathmicChatbot/TwitchManager.cs
@@ -1,4 +1,6 @@
-﻿using DeathmicChatbot.StreamInfo;
+﻿using System;
+using System.IO;
+using DeathmicChatbot.StreamInfo;
 using RestSharp;
 using RestSharp.Deserializers;
 using System.Collections.Generic;
@@ -10,10 +12,26 @@ namespace DeathmicChatbot
         private readonly List<string> _streams;
         private readonly RestClient _client;
 
+        private const string STREAMS_FILE = "streams.txt";
+
         public TwitchManager()
         {
             _client = new RestClient("https://api.twitch.tv");
             _streams = new List<string>();
+
+            LoadStreams();
+        }
+
+        private void LoadStreams()
+        {
+            if (!File.Exists(STREAMS_FILE)) File.Create(STREAMS_FILE).Close();
+            StreamReader reader = new StreamReader(STREAMS_FILE);
+            while (!reader.EndOfStream)
+            {
+                string sLine = reader.ReadLine();
+                if (!_streams.Contains(sLine)) _streams.Add(sLine);
+            }
+            reader.Close();
         }
 
         public RootObject GetOnlineStreams()
@@ -30,12 +48,24 @@ namespace DeathmicChatbot
 
         public void AddStream(string stream)
         {
-            _streams.Add(stream);
+            if (!_streams.Contains((stream))) _streams.Add(stream);
+            WriteStreamsToFile();
         }
 
         public void RemoveStream(string stream)
         {
             _streams.Remove(stream);
+            WriteStreamsToFile();
+        }
+
+        private void WriteStreamsToFile()
+        {
+            StreamWriter writer = new StreamWriter(STREAMS_FILE, false);
+            foreach (string stream in _streams)
+            {
+                writer.WriteLine(stream);
+            }
+            writer.Close();
         }
 
         private static string ArrayToString(IEnumerable<string> arr)
