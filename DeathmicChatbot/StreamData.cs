824f0847ccfd647608a503117ec8139e9b833704
Deathmic <deathmic@gmail.com>
2013-08-17 00:15:56 +0200

    Added more useful chat messages about streams

--- DeathmicChatbot/StreamData.cs
+++ DeathmicChatbot/StreamData.cs
@@ -8,5 +8,10 @@ namespace DeathmicChatbot
         public Stream Stream { get; set; }
 
         public DateTime Started { get; set; }
+
+        public TimeSpan TimeSinceStart
+        {
+            get { return DateTime.Now - Started; }
+        }
     }
 }
