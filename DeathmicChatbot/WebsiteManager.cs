bc35fcd3632658b11b42c6d5e15669015664a50a
Beliar <KarstenBock@gmx.net>
2013-08-16 16:22:11 +0200

    links to i.imgur are now converted into an url where a title maybe read from.

--- DeathmicChatbot/WebsiteManager.cs
+++ DeathmicChatbot/WebsiteManager.cs
@@ -12,6 +12,7 @@ namespace DeathmicChatbot
     internal class WebsiteManager
     {
         private readonly Regex _reg;
+		private readonly Regex _imgurreg;
         private readonly LogManager _log;
 
         public WebsiteManager(LogManager log)
@@ -21,6 +22,7 @@ namespace DeathmicChatbot
             _reg =
                 new Regex(
                     @"(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?");
+			_imgurreg = new Regex(@"(https?:\/\/)?i.imgur.com\/(\w+).\w+");
             _log = log;
         }
 
@@ -51,6 +53,12 @@ namespace DeathmicChatbot
         {
             try
             {
+				Match mt = _imgurreg.Match (url);
+				if (mt.Success)
+				{
+					string id = mt.Groups [2].Value;
+					url = "http://imgur.com/gallery/" + id;
+				}
 				var webGet = new HtmlWeb();
                 HtmlDocument doc;
                 try
