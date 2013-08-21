b1f9a84c732385f35fc801883d847e432568eefa
Kolpa <kolyaopahle@gmail.com>
2013-08-21 14:18:14 +0200

    Update StreamInfo.cs

--- DeathmicChatbot/StreamInfo.cs
+++ DeathmicChatbot/StreamInfo.cs
@@ -2,75 +2,75 @@
 
 namespace DeathmicChatbot.StreamInfo
 {
-    public class Links
-    {
-        public string Videos { get; set; }
-        public string Subscriptions { get; set; }
-        public string Features { get; set; }
-        public string Commercial { get; set; }
-        public string Follows { get; set; }
-        public string Chat { get; set; }
-        public string Editors { get; set; }
-        public string Teams { get; set; }
-        public string Self { get; set; }
-        public string Stream_Key { get; set; }
-    }
-
-    public class Channel
-    {
-        public object Profile_Banner { get; set; }
-        public string Created_At { get; set; }
-        public string Updated_At { get; set; }
-        public string Status { get; set; }
-        public bool Mature { get; set; }
-        public int ID { get; set; }
-        public string Logo { get; set; }
-        public object Banner { get; set; }
-        public string Url { get; set; }
-        public string Game { get; set; }
-        public string Background { get; set; }
-        public string Name { get; set; }
-        public int Delay { get; set; }
-        public string Display_Name { get; set; }
-        public string Video_Banner { get; set; }
-        public Links Links { get; set; }
-    }
-
     public class Preview
     {
-        public string Medium { get; set; }
-        public string Large { get; set; }
-        public string Small { get; set; }
-        public string Template { get; set; }
+        public string small { get; set; }
+        public string medium { get; set; }
+        public string large { get; set; }
+        public string template { get; set; }
+    }
+
+    public class Links
+    {
+        public string self { get; set; }
     }
 
     public class Links2
     {
-        public string Self { get; set; }
+        public string subscriptions { get; set; }
+        public string editors { get; set; }
+        public string stream_key { get; set; }
+        public string videos { get; set; }
+        public string self { get; set; }
+        public string follows { get; set; }
+        public string features { get; set; }
+        public string teams { get; set; }
+        public string commercial { get; set; }
+        public string chat { get; set; }
+    }
+
+    public class Channel
+    {
+        public string url { get; set; }
+        public string background { get; set; }
+        public string video_banner { get; set; }
+        public string status { get; set; }
+        public int delay { get; set; }
+        public object mature { get; set; }
+        public string display_name { get; set; }
+        public int _id { get; set; }
+        public string updated_at { get; set; }
+        public Links2 _links { get; set; }
+        public string banner { get; set; }
+        public string created_at { get; set; }
+        public string game { get; set; }
+        public string name { get; set; }
+        public object profile_banner { get; set; }
+        public string logo { get; set; }
     }
 
     public class Stream
     {
-        public int Viewers { get; set; }
-        public long ID { get; set; }
-        public Channel Channel { get; set; }
-        public string Game { get; set; }
-        public Preview Preview { get; set; }
-        public Links2 Links { get; set; }
+        public Preview preview { get; set; }
+        public long _id { get; set; }
+        public Links _links { get; set; }
+        public string game { get; set; }
+        public int viewers { get; set; }
+        public Channel channel { get; set; }
     }
 
     public class Links3
     {
-        public string Featured { get; set; }
-        public string Next { get; set; }
-        public string Followed { get; set; }
-        public string Self { get; set; }
-        public string Summary { get; set; }
+        public string summary { get; set; }
+        public string next { get; set; }
+        public string featured { get; set; }
+        public string self { get; set; }
+        public string followed { get; set; }
     }
 
     public class RootObject
     {
-        public List<Stream> Streams { get; set; }
-        public Links3 Links { get; set; }
+        public List<Stream> streams { get; set; }
+        public Links3 _links { get; set; }
     }
 }
