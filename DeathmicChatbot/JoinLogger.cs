580caae1da3512b5aa2b72d074f9a7449b7b4597
chromos33 <benjamin@kreft.com>
2015-03-03 22:26:53 +0100

    1

--- DeathmicChatbot/JoinLogger.cs
+++ DeathmicChatbot/JoinLogger.cs
@@ -55,7 +55,15 @@ namespace DeathmicChatbot
 
             string[] userdata = xmlprovider.UserInfo(sNick).Split(',');
             String days_since_last_visit = DateTime.Now.Subtract(Convert.ToDateTime(userdata[1])).ToString("d' days 'h':'mm':'ss");
-
+            string visitstring = "";
+            switch(userdata[1])
+            {
+                case "1": visitstring = userdata[1] +" st"; break;
+                case "2": visitstring = userdata[1] +" nd"; break;
+                case "3": visitstring = userdata[1] +" rd"; break;
+                default: visitstring = userdata[1] +" th"; break;
+            }
+            // Userdata[0] = VisitCount , Userdata[1] = LastVisit
             return string.Format("This is {0}'s {1} visit." + System.Environment.NewLine + "Their last visit was on {3} ({4} ago)", sNick, userdata[0], userdata[1], days_since_last_visit);
 
             /*
