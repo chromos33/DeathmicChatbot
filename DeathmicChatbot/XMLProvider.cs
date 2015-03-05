8c9d691576b8c22b274513bf66d68500fe6d7d9a
chromos33 <benjamin@kreft.com>
2015-03-05 23:47:42 +0100

    Merge Conflict resolved

--- DeathmicChatbot/XMLProvider.cs
+++ DeathmicChatbot/XMLProvider.cs
@@ -380,7 +380,6 @@ namespace DeathmicChatbot
                                     System.Diagnostics.Debug.WriteLine("streamstart");
                                     stream.Attribute("starttime").Value = DateTime.Now.ToString();
                                 }
-                                stream.Attribute("starttime").Value = DateTime.Now.ToString();
 
                                 stream.Attribute("running").Value = "true";
                             }
