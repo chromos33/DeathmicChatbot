d84f96e4bf68f4710b878b2800a183510890ff7c
chromos33 <benjamin@kreft.com>
2015-04-04 00:28:39 +0200

    quick filename fix

--- BotDeathmic/StreamInfo/stream.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DeathmicChatbot.StreamInfo
-{
-    public class Stream
-    {
-        public string Channel { get; set; }
-        public string Message { get; set; }
-        public string Game { get; set; }
-    }
-}
