8fccb1bb049c9d431247518928b9b59b89b38142
chromos33 <benjamin@kreft.com>
2015-04-09 22:58:34 +0200

    ...

--- DeathmicChatbot/StreamInfo/Twitch/TwitchProvider.cs
+++ DeathmicChatbot/StreamInfo/Twitch/TwitchProvider.cs
@@ -175,8 +175,7 @@ namespace DeathmicChatbot.StreamInfo.Twitch
             var req = new RestRequest("/kraken/streams", Method.GET);
             req.AddParameter("channel", ArrayToString(_streams));
             var response = _client.Execute(req);
-            Console.WriteLine(response.ErrorException);
-            Console.WriteLine(response.ErrorMessage);
+            Console.WriteLine(ArrayToString(_streams));
             try
             {
                 var des = new JsonDeserializer();
