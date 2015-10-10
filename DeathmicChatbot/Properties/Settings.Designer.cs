ae6c3938f68186250a81e43f3fddd67d1b140144
Benjamin Kreft <benjamin@kreft.com>
2015-10-10 21:01:11 +0200

    added ResetStreamState function

--- DeathmicChatbot/Properties/Settings.Designer.cs
+++ DeathmicChatbot/Properties/Settings.Designer.cs
@@ -79,7 +79,7 @@ namespace DeathmicChatbot.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("bobdeathmic")]
+        [global::System.Configuration.DefaultSettingValueAttribute("BobDeathmic")]
         public string Name {
             get {
                 return ((string)(this["Name"]));
