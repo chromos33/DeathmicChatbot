d38661dcbe54df4a857f9e6ccdc5d05c54f3c4f1
Benjamin Kreft <benjamin@kreft.com>
2016-02-05 20:49:48 +0100

    initial commit

---
+++ DungeonsAndDragonsBattleSimulator/DungeonsAndDragonsBattleSimulator/Classes/SubClasses/Point.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragonsBattleSimulator.Classes.SubClasses
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        private bool filled;
+        public Point(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public bool Filled
+        {
+            get { return filled; }
+        }
+        public void togglefilled()
+        {
+            filled = !filled;
+        }
+        public double Distance(Point _goal)
+        {
+            return Math.Sqrt(Math.Pow(((double)_goal.X - (double)X), 2) + Math.Pow(((double)_goal.Y - (double)Y), 2));
+        }
+        public bool isAdjacent(Point _goal)
+        {
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if(X-i == _goal.X && Y-j == _goal.Y)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ DungeonsAndDragonsBattleSimulator/DungeonsAndDragonsBattleSimulator/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DungeonsAndDragonsBattleSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MovePoints = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // MovePoints
+            // 
+            this.MovePoints.FormattingEnabled = true;
+            this.MovePoints.Location = new System.Drawing.Point(12, 12);
+            this.MovePoints.Name = "MovePoints";
+            this.MovePoints.Size = new System.Drawing.Size(245, 212);
+            this.MovePoints.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.MovePoints);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox MovePoints;
+    }
+}
+
