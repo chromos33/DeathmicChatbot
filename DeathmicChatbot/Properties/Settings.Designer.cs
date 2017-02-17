4df2a545278482674e55eead76884a04d27c96cf
Benja <Benja@DESKTOP-A359P2J>
2017-02-17 18:53:48 +0100

    Changed twitch query interval

--- DeathmicChatbot/Properties/Settings.Designer.cs
+++ DeathmicChatbot/Properties/Settings.Designer.cs
@@ -34,7 +34,7 @@ namespace DeathmicChatbot.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("15")]
+        [global::System.Configuration.DefaultSettingValueAttribute("3")]
         public int StreamcheckIntervalSeconds {
             get {
                 return ((int)(this["StreamcheckIntervalSeconds"]));
