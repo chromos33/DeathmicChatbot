ccd7538a2ecbfd0af8271edf1a2906bdd3e33ce5
Benjamin Kreft <benjamin@kreft.com>
2018-08-02 21:15:54 +0200

    removed ; after if statement...

--- BobDeathmic/Services/DiscordService.cs
+++ BobDeathmic/Services/DiscordService.cs
@@ -48,7 +48,7 @@ namespace BobDeathmic.Services
 
         private void StreamChanged(object sender, StreamEventArgs e)
         {
-            if (e.StreamType == Models.Enum.StreamProviderTypes.Mixer && e.state == Models.Enum.StreamState.Started) ;
+            if (e.StreamType == Models.Enum.StreamProviderTypes.Mixer && e.state == Models.Enum.StreamState.Started)
             {
                 NotifySubscriber(e);
             }
